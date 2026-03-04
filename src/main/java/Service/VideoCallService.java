e082e2fe7b9e6c8e4a413a0d5d6f7242a1d841bc
Rabii khenissi <rabiikhenissi124@gmail.com>
2026-03-04 06:20:05 +0100

    fixing errors

--- src/main/java/Service/VideoCallService.java
+++ src/main/java/Service/VideoCallService.java
@@ -9,9 +9,7 @@ public class VideoCallService {
 
     public static String generateMeetingLink(int appointmentId) {
         String roomId = "session-" + appointmentId;
-        // meet.jit.si now requires login to start a meeting. Using a community instance
-        // (e.g., Init7) that allows anonymous users
-        return "https://meet.init7.net/" + roomId;
+        return "https://meet.jit.si/" + roomId;
     }
 
     public static boolean isTimeForAppointment(LocalDateTime appointmentTime) {
